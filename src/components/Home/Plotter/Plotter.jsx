608e683078cbb29af7760b881ced2c15d62b9cc8
Alyorbekjonxon <alyorbek391@gmail.com>
2024-11-21 20:05:02 +0500

    alalla

--- src/components/Home/Plotter/Plotter.jsx
+++ src/components/Home/Plotter/Plotter.jsx
@@ -1,39 +1,44 @@
-import React from 'react'
-import "./Plotter.scss"
+import React from "react";
+import "./Plotter.scss";
 
 const Plotter = () => {
   return (
-    <div className='Plotter'>
-       
-       <div className="container">
-          
-          <div className="Plotter-parent">
+    <div className="Plotter">
+      <div className="container">
+        <div className="Plotter-parent">
+          {/* <img src="" alt="" /> */}
 
-            {/* <img src="" alt="" /> */}
-             
-             <div className="ploter-backed">
-                <div className="blur">
+          <div className="ploter-backed">
+            <div className="blur"></div>
 
-                </div>
-
-                <div className="bac1">
-                    <img src="./Plotter/fourthbanner-nAd0r_eJ.png" alt="" />
-                </div>
-                <div className="bac2">
-                    <img src="./Plotter/fourthbannerprint-MywiEDUR.png" alt="" />
-                </div>
-             </div>
-             <div className="ploter-frond">
-              
-             <h1>Ремонт Plotter принтеровс гарантией качества!</h1>
-
-             </div>
+            <div className="bac1">
+              <img src="./public/Plotter/fourthbanner-nAd0r_eJ.png" alt="" />
+            </div>
+            <div className="bac2">
+              <img
+                src="./public/Plotter/fourthbannerprint-MywiEDUR.png"
+                alt=""
+              />
+            </div>
           </div>
+          <div className="ploter-frond">
+            <h1>
+              Ремонт Plotter принтеров с <br /> гарантией качества!{" "}
+            </h1>
 
-       </div>
-
+            <p>
+              Мы обеспечим надежный ремонт и обслуживание вашего Plotter
+              принтера, чтобы он снова работал как новый.
+            </p>
+          </div>
+        </div>
+       
+      </div>
+       <div className="Plotter-backed-img">
+         <img src="public/Plotter/fixed-fon-CsSjUvgT.jpg" alt="" />
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Plotter
+export default Plotter;
--- src/i18n.jsx
+++ src/i18n.jsx
@@ -2,9 +2,9 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
-import uzTranslation from "../locales/uz.json";
-import enTranslation from "../locales/en.json";
-import ruTranslation from "../locales/ru.json";
+import uzTranslation from "../public/locales/uz.json";
+import enTranslation from "../public/locales/en.json";
+import ruTranslation from "../public/locales/ru.json";
 
 i18n
 .use(Backend)
